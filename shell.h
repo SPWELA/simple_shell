a3e98aab0ab9a9b5276c5891de52585046e6b86c
SPWELA <sindisiwemp@gmail.com>
2022-08-10 15:58:53 -0700

    split

--- shell.h
+++ shell.h
@@ -68,6 +68,13 @@ char *check_path(char *);
 void (*get_func(char *))(char **);
 char *_getenv(char *);
 
+/* split.c */
+char *swap_char(char *input, int bool);
+void add_nodes(sep_list **head_s, line_list **head_l, char *input);
+void go_next(sep_list **list_s, line_list **list_l, data_shell *datash);
+int split_commands(data_shell *datash, char *input);
+char **split_line(char *input);
+
 /*built_in*/
 void env(char **);
 void quit(char **);
